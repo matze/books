059012b7f7de17dfaefdd9e4f29771f557284a70
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2012-11-03 10:19:58 +0100

    Remove unused class methods

--- src/books-window.c
+++ src/books-window.c
@@ -127,46 +127,11 @@ books_window_dispose (GObject *object)
 }
 
 static void
-books_window_finalize (GObject *object)
-{
-    G_OBJECT_CLASS (books_window_parent_class)->finalize (object);
-}
-
-static void
-books_window_set_property (GObject *object,
-                         guint property_id,
-                         const GValue *value,
-                         GParamSpec *pspec)
-{
-    switch (property_id) {
-        default:
-            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-            break;
-    }
-}
-
-static void
-books_window_get_property(GObject *object,
-                        guint property_id,
-                        GValue *value,
-                        GParamSpec *pspec)
-{
-    switch (property_id) {
-        default:
-            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-            break;
-    }
-}
-
-static void
 books_window_class_init (BooksWindowClass *klass)
 {
     GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
-    object_class->set_property = books_window_set_property;
-    object_class->get_property = books_window_get_property;
     object_class->dispose = books_window_dispose;
-    object_class->finalize = books_window_finalize;
 
     g_type_class_add_private (klass, sizeof(BooksWindowPrivate));
 }
